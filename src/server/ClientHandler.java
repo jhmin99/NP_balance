406b5af6c4b2759143257099e13ce4c6790b5db8
Jay Park <pjh6183@naver.com>
2024-11-26 14:02:57 +0900

    sever method complete

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -9,6 +9,7 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.Socket;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -43,12 +44,12 @@ public class ClientHandler extends Thread {
                 switch (action) {
 
                     case "REGISTER","LOGIN" -> handleUserAuth(action);  // 성공, 실패 응답
-//                    case "LOGOUT"                                     // server.ClientHandler username 초기화 및 응답
+                    case "LOGOUT" -> handleLogout();                    // server.ClientHandler username 초기화 및 응답
 
                     case "GAME_LIST" -> sendAllGameList();              // Game 리스트 (전체) 응답
-//                    case "GAME_LIST_VOTED" -> sendGameListVoted();    // Game 리스트 (playedGameIds 필터링) 응답
-//                    case "GAME_LIST_ID" -> sendGameListById();        // Game 리스트 (createdGameIds 필터링) 응답
-//
+                    case "GAME_LIST_VOTED" -> sendGameListVoted();    // Game 리스트 (playedGameIds 필터링) 응답
+                    case "GAME_LIST_ID" -> sendGameListById();        // Game 리스트 (createdGameIds 필터링) 응답
+
                     case "USER_LIST" -> sendAllUserList();               // User 해시맵 (전체) 응답
 
                     case "ADD_GAME" -> addGame();                       // 성공, 실패 응답
@@ -62,7 +63,8 @@ public class ClientHandler extends Thread {
                     case "VOTE" -> handleVote();                        // GAME 객체 브로드캐스트 또는 분리
                     case "CHAT" -> handleChat();                        // GAME 객체 브로드캐스트 또는 분리
                     
-                    // case "LIKE_CHAT" -> 이것도 만들어서 각 Comment 객체의 likes 를 수정하도록 해줘야 함
+                     case "LIKE_CHAT" -> handleLikeChat();
+                    // -> 이것도 만들어서 각 Comment 객체의 likes 를 수정하도록 해줘야 함
                     //                  -> 당연하게도, like 이후에는 위처럼 broadcast 처리가 되어야 한다???
 
                     default -> {
@@ -93,6 +95,22 @@ public class ClientHandler extends Thread {
         }
     }
 
+    private void handleLogout() throws IOException {
+        if (username == null) {
+            synchronized (output) {
+                output.writeObject("LOGOUT_FAIL");
+                output.flush();
+            }
+            return;
+        }
+        username = null;
+
+        synchronized (output) {
+            output.writeObject("LOGOUT_SUCCESS");
+            output.flush();
+        }
+    }
+
     private void sendAllGameList() throws IOException {
         ConcurrentHashMap<String, Game> allGames = gameManager.getAllGames();
 
@@ -103,12 +121,86 @@ public class ClientHandler extends Thread {
         }
     }
 
+    private void sendGameListVoted() throws IOException {
+        // 유저가 아직 로그인을 안 했을 경우
+        if (username == null) {
+            synchronized (output) {
+                output.writeObject("GAME_LIST_VOTED_FAIL");
+                output.flush();
+            }
+            return;
+        }
+
+        User currentUser = userManager.findUserByName(username);
+        // 현재 유저가 없을 경우
+        if (currentUser == null) {
+            synchronized (output) {
+                output.writeObject("GAME_LIST_VOTED_FAIL");
+                output.flush();
+            }
+            return;
+        }
+
+        List<String> playedGameIds = currentUser.getPlayedGameIds();
+        ConcurrentHashMap<String, Game> allGames = gameManager.getAllGames();
+        ConcurrentHashMap<String, Game> votedGames = new ConcurrentHashMap<>();
+
+        for (String gameId : playedGameIds) {
+            if (allGames.containsKey(gameId)) {
+                votedGames.put(gameId, allGames.get(gameId));
+            }
+        }
+
+        synchronized (output) {
+            output.writeObject("GAME_LIST_VOTE_SUCCESS");
+            output.writeObject(votedGames);
+            output.flush();
+        }
+    }
+
+    private void sendGameListById() throws IOException {
+        // 유저가 아직 로그인을 안 했을 경우
+        if (username == null) {
+            synchronized (output) {
+                output.writeObject("GAME_LIST_VOTED_FAIL");
+                output.flush();
+            }
+            return;
+        }
+
+        User currentUser = userManager.findUserByName(username);
+        // 현재 유저가 없을 경우
+        if (currentUser == null) {
+            synchronized (output) {
+                output.writeObject("GAME_LIST_VOTED_FAIL");
+                output.flush();
+            }
+            return;
+        }
+
+        List<String> createdGameIds = currentUser.getCreatedGameIds();
+        ConcurrentHashMap<String, Game> allGames = gameManager.getAllGames();
+        ConcurrentHashMap<String, Game> createdGames = new ConcurrentHashMap<>();
+
+        for (String gameId : createdGameIds) {
+            if (allGames.containsKey(gameId)) {
+                createdGames.put(gameId, allGames.get(gameId));
+            }
+        }
+
+        synchronized (output) {
+            output.writeObject("GAME_LIST_ID_SUCCESS");
+            output.writeObject(createdGames);
+            output.flush();
+        }
+    }
+
     private void sendAllUserList() throws IOException {
-        ConcurrentHashMap<String, User> allusers = userManager.getAllUsers();
+        ConcurrentHashMap<String, User> allUsers = userManager.getAllUsers();
 
         synchronized (output) {
             output.writeObject("USER_LIST_SUCCESS");
-            output.writeObject(allusers);
+            output.writeObject(allUsers);
             output.flush();
         }
     }
@@ -256,6 +348,26 @@ public class ClientHandler extends Thread {
         }catch (Exception e){
             synchronized (output) {
                 output.writeObject("CHAT_FAIL");
+                output.flush();
+            }
+        }
+    }
+
+    private void handleLikeChat() throws IOException, ClassNotFoundException {
+        String gameId = (String) input.readObject();
+        int commentId = (int) input.readObject();
+
+        try {
+            gameManager.addLikeComment(gameId, commentId);
+
+            synchronized (output) {
+                output.writeObject("LIKE_CHAT_SUCCESS");
+                output.flush();
+                broadcastGameDetails();
+            }
+        } catch (Exception e) {
+            synchronized (output) {
+                output.writeObject("LIKE_CHAT_FAIL");
                 output.flush();
             }
         }
